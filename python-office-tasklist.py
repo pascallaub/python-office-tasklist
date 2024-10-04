b8c3851f512d03a4d894bbe38a09c23f78b09ff9
pascallaub <nutritionexpertition@gmail.com>
2024-10-04 15:56:20 +0200

    implemented sorted function

--- python-office-tasklist.py
+++ python-office-tasklist.py
@@ -1,14 +1,26 @@
 tasklist = []
 
+priority_order = {
+    "gering": 1,
+    "mittel": 2,
+    "hoch": 3
+}
+
 def task_input():
     task = input("Welche Aufgabe möchtest du erledigen: ")
     date = input("Fälligkeitsdatum: ")
+    prio = input("Priorität (gering, mittel, hoch): ")
 
-    tasklist.append((task, date))
+    tasklist.append((task, date, prio))
+
+def sorted_prio():
+    sorted_list = sorted(tasklist, key=lambda x: priority_order[x[2]], reverse=True) # Sortiert nach prio und vergleicht die dritte Stelle mit priority_order
+    for task in sorted_list:
+        print(task)
     
 
 def show_tasks():
-    if tasklist == '':
+    if tasklist == []:
         print("Die Liste ist leer.")
     else:
         for item in tasklist:
@@ -16,13 +28,15 @@ def show_tasks():
 
 def menu():
     while True:
-        choice = input("Aufgaben hinzufügen (1), Aufgaben anzeigen (2), Beenden (3): ")
+        choice = input("Aufgaben hinzufügen (1), Aufgaben anzeigen (2), Aufgaben sortieren (3), Beenden (4): ")
 
         if choice == '1':
             task_input()
         elif choice == '2':
             show_tasks()
         elif choice == '3':
+            sorted_prio()
+        elif choice == '4':
             quit()
         else:
             "Falsche Eingabe!"
